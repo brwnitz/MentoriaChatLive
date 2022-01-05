593007954f724871710e08b9b8b2055a0c1ad533
Bruno Torres <brunote16@hotmail.com>
2022-01-05 14:00:51 -0300

    Adicionar arquivos de projeto.

---
+++ Hubs/ILiveChatHub.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MentoriaChatLive.Hubs
+{
+    public interface ILiveChatHub
+    {
+        Task OnExitChatAsync(string username);
+        Task OnEnterChatAsync(string username);
+        Task OnNewMessageAsync(string username, string message);
+    }
+}
---
+++ Hubs/LiveChatHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace MentoriaChatLive.Hubs
+{
+    public class LiveChatHub : Hub<ILiveChatHub>, ILiveChatHub
+    {
+        private const string LiveChatGroup = "LiveChatGroup";
+        public override async Task OnConnectedAsync(){
+            await Groups.AddToGroupAsync(Context.ConnectionId, LiveChatGroup);
+        }
+        public override async Task OnDisconnectedAsync(System.Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, LiveChatGroup);
+        }
+
+        public async Task OnEnterChatAsync(string username)
+        {
+            await Clients.Groups(LiveChatGroup).OnEnterChatAsync(username);
+        }
+
+        public async Task OnExitChatAsync(string username)
+        {
+            await Clients.OthersInGroup(LiveChatGroup).OnExitChatAsync(username);
+        }
+
+        public async Task OnNewMessageAsync(string username, string message)
+        {
+            await Clients.OthersInGroup(LiveChatGroup).OnNewMessageAsync(username, message);
+        }
+    }
+}
